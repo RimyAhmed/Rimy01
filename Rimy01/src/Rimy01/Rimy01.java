5e01739c59a601202980b7dee3d5c46aa197383e
RimyAhmed <33205756+RimyAhmed@users.noreply.github.com>
2017-11-05 14:01:30 -0500

    Update Rimy01.java

--- Rimy01/src/Rimy01/Rimy01.java
+++ Rimy01/src/Rimy01/Rimy01.java
@@ -3,6 +3,7 @@ package Rimy01;
 public class Rimy01 {
 	public static void main(String[]args){
 		System.out.println("This is my first GIT project");
+		System.out.println("github is github");
 	}
 
 }
